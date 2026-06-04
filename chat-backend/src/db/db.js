1a391d91528a8366584ea9ef451867d1c8d27e35
n1vers <nikita.andronov@ivkhk.ee>
2026-06-04 11:15:26 +0300

    wokr2

--- chat-backend/src/db/db.js
+++ chat-backend/src/db/db.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 async function connectDB() {
   try {
     const mongoUri = process.env.MONGO_URI;
-
+    
     if (!mongoUri) {
       throw new Error("MONGO_URI is not defined in .env");
     }
 
-    await mongoose.connect(mongoUri);
+    // Передаем объект с настройкой family: 4 вторым аргументом
+    await mongoose.connect(mongoUri, {
+      family: 4
+    });
 
     console.log("MongoDB connected");
   } catch (error) {
